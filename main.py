7408d1578961af481a649583ea56efd8582c549b
Carlos William W <99049613+CarlosWTMachado@users.noreply.github.com>
2022-02-19 07:18:07 +0000

    Anao

--- main.py
+++ main.py
@@ -3,134 +3,43 @@ import os
 from random import choice
 from keep_alive import keep_alive
 
-agents = [{
-    'nome': 'Neon',
-    'image': 'imagens/neon.jpg', 
-    'gif': 'https://tenor.com/bOunQ.gif'
-}, {
-    'nome': 'Chamber',
-    'image': 'imagens/xambinho.jpg', 
-    'gif': 'https://tenor.com/bPeJp.gif'
-}, {
-    'nome': 'Skye',
-    'image': 'imagens/skye.jpg', 
-    'gif': 'https://tenor.com/bqRnS.gif'
-}, {
-    'nome': 'Yoru',
-    'image': 'imagens/yoru.png', 
-    'gif': 'https://tenor.com/bvCRZ.gif'
-}, {
-    'nome': 'Astra',
-    'image': 'imagens/astra.png', 
-    'gif': 'https://tenor.com/bFdL7.gif'
-}, {
-    'nome': 'KAY/O',
-    'image': 'imagens/caio.jpg', 
-    'gif': 'https://tenor.com/bFHwm.gif'
-}, {
-    'nome': 'Fenix',
-    'image': 'imagens/fenix.png', 
-    'gif': 'https://tenor.com/bQayi.gif'
-}, {
-    'nome': 'Raze',
-    'image': 'imagens/raize.png', 
-    'gif': 'https://tenor.com/bNtSD.gif'
-}, {
-    'nome': 'Brimstone',
-    'image': 'imagens/brimstone.jpg', 
-    'gif': 'https://tenor.com/bvbtQ.gif'
-}, {
-    'nome': 'Jett',
-    'image': 'imagens/jett.jpg', 
-    'gif': 'https://tenor.com/bEHcn.gif'
-}, {
-    'nome': 'Sage',
-    'image': 'imagens/sage.jpg', 
-    'gif': 'https://tenor.com/bHhhf.gif'
-}, {
-    'nome': 'Viper',
-    'image': 'imagens/viper.png', 
-    'gif': 'https://tenor.com/bLlJA.gif'
-}, {
-    'nome': 'Breach',
-    'image': 'imagens/breach.png', 
-    'gif': 'https://tenor.com/bJmAo.gif'
-}, {
-    'nome': 'Cypher',
-    'image': 'imagens/cypher.jpg', 
-    'gif': 'https://tenor.com/bovkI.gif'
-}, {
-    'nome': 'Sova',
-    'image': 'imagens/sova.jpg', 
-    'gif': 'https://tenor.com/bCbue.gif'
-}, {
-    'nome': 'Homem',
-    'image': 'imagens/omem.jpg', 
-    'gif': 'https://tenor.com/bD7OT.gif'
-}, {
-    'nome': 'Reyna',
-    'image': 'imagens/reina.jpg', 
-    'gif': 'https://tenor.com/bO4GA.gif'
-}, {
-    'nome': 'Killjoy',
-    'image': 'imagens/killjoy.png', 
-    'gif': 'https://tenor.com/bDx6X.gif'
-}]
+agents = [{'nome': 'Neon', 'image': 'imagens/neon.jpg', 'gif': 'https://tenor.com/bOunQ.gif'}, 
+{'nome': 'Chamber', 'image': 'imagens/xambinho.jpg', 'gif': 'https://tenor.com/bPeJp.gif'}, 
+{'nome': 'Skye', 'image': 'imagens/skye.jpg', 'gif': 'https://tenor.com/bqRnS.gif'}, 
+{'nome': 'Yoru', 'image': 'imagens/yoru.png', 'gif': 'https://tenor.com/bvCRZ.gif'}, 
+{'nome': 'Astra', 'image': 'imagens/astra.png', 'gif': 'https://tenor.com/bFdL7.gif'}, 
+{'nome': 'KAY/O', 'image': 'imagens/caio.jpg', 'gif': 'https://tenor.com/bFHwm.gif'}, 
+{'nome': 'Fenix', 'image': 'imagens/fenix.png', 'gif': 'https://tenor.com/bQayi.gif'}, 
+{'nome': 'Raze', 'image': 'imagens/raize.png', 'gif': 'https://tenor.com/bNtSD.gif'}, 
+{'nome': 'Brimstone', 'image': 'imagens/brimstone.jpg', 'gif': 'https://tenor.com/bvbtQ.gif'}, 
+{'nome': 'Jett', 'image': 'imagens/jett.jpg', 'gif': 'https://tenor.com/bEHcn.gif'}, 
+{'nome': 'Sage', 'image': 'imagens/sage.jpg', 'gif': 'https://tenor.com/bHhhf.gif'}, 
+{'nome': 'Viper', 'image': 'imagens/viper.png', 'gif': 'https://tenor.com/bLlJA.gif'}, 
+{'nome': 'Breach', 'image': 'imagens/breach.png', 'gif': 'https://tenor.com/bJmAo.gif'}, 
+{'nome': 'Cypher', 'image': 'imagens/cypher.jpg', 'gif': 'https://tenor.com/bovkI.gif'}, 
+{'nome': 'Sova', 'image': 'imagens/sova.jpg', 'gif': 'https://tenor.com/bCbue.gif'}, 
+{'nome': 'Homem', 'image': 'imagens/omem.jpg', 'gif': 'https://tenor.com/bD7OT.gif'}, 
+{'nome': 'Reyna', 'image': 'imagens/reina.jpg', 'gif': 'https://tenor.com/bO4GA.gif'}, 
+{'nome': 'Killjoy', 'image': 'imagens/killjoy.png', 'gif': 'https://tenor.com/bDx6X.gif'}]
 
-armas = [{
-    'nome': 'Spectre',
-    'image': 'imagens/spectre.webp'
-}, {
-    'nome': 'Odin',
-    'image': 'imagens/odin.jpg'
-}, {
-    'nome': 'Vandal',
-    'image': 'imagens/vandal.jpg'
-}, {
-    'nome': 'Bucky',
-    'image': 'imagens/bucky.webp'
-}, {
-    'nome': 'Bulldog',
-    'image': 'imagens/bulldog.webp'
-}, {
-    'nome': 'Operator',
-    'image': 'imagens/operator.jpg'
-}, {
-    'nome': 'Stinger',
-    'image': 'imagens/stinger.webp'
-}, {
-    'nome': 'Guardian',
-    'image': 'imagens/guardian.jpg'
-}, {
-    'nome': 'Ares',
-    'image': 'imagens/ares.jpg'
-}, {
-    'nome': 'Phantom',
-    'image': 'imagens/phantom.jpg'
-}, {
-    'nome': 'Marhsall',
-    'image': 'imagens/marshall.jpg'
-}, {
-    'nome': 'Judge',
-    'image': 'imagens/judge.jpg'
-}]
+armas = [{'nome': 'Spectre', 'image': 'imagens/spectre.webp'}, 
+{'nome': 'Odin', 'image': 'imagens/odin.jpg'}, 
+{'nome': 'Vandal', 'image': 'imagens/vandal.jpg'}, 
+{'nome': 'Bucky', 'image': 'imagens/bucky.webp'}, 
+{'nome': 'Bulldog', 'image': 'imagens/bulldog.webp'}, 
+{'nome': 'Operator', 'image': 'imagens/operator.jpg'}, 
+{'nome': 'Stinger', 'image': 'imagens/stinger.webp'}, 
+{'nome': 'Guardian', 'image': 'imagens/guardian.jpg'}, 
+{'nome': 'Ares', 'image': 'imagens/ares.jpg'}, 
+{'nome': 'Phantom', 'image': 'imagens/phantom.jpg'}, 
+{'nome': 'Marhsall', 'image': 'imagens/marshall.jpg'}, 
+{'nome': 'Judge', 'image': 'imagens/judge.jpg'}]
 
-pistolas = [{
-    'nome': 'Classic',
-    'image': 'imagens/classic.webp'
-}, {
-    'nome': 'Shorty',
-    'image': 'imagens/shorty.jpg'
-}, {
-    'nome': 'Frenzy',
-    'image': 'imagens/frenzy.webp'
-}, {
-    'nome': 'Ghost',
-    'image': 'imagens/ghost.webp'
-}, {
-    'nome': 'Sheriff',
-    'image': 'imagens/sheriff.webp'
-}]
+pistolas = [{'nome': 'Classic', 'image': 'imagens/classic.webp'}, 
+{'nome': 'Shorty', 'image': 'imagens/shorty.jpg'}, 
+{'nome': 'Frenzy', 'image': 'imagens/frenzy.webp'}, 
+{'nome': 'Ghost', 'image': 'imagens/ghost.webp'}, 
+{'nome': 'Sheriff', 'image': 'imagens/sheriff.webp'}]
 
 valorant = ["valorant", "valorantes", "@valorantes"]
 
@@ -161,11 +70,17 @@ async def on_message(message):
     if message.content == '$help':
       await info(message)
 
-    if message.content.lower().find('valorant') > -1 or os.getenv("RoleValorantes") in message.content:
+    if message.content.lower().find('valorant') > -1 or message.content.lower().find('vava') > -1 or os.getenv("RoleValorantes") in message.content:
       await message.reply(content= f"Opa! Valorant? bora, logando... \n{os.getenv('RoleValorantes')}\nhttps://tenor.com/view/hasbulla-gif-22466319")
 
     if client.user.mentioned_in(message) and message.mention_everyone is False:
       await message.reply('O que tu quer?')
+
+    if (message.content.lower().find('anao') > -1 or 
+    message.content.lower().find('anão') > -1 or 
+    message.content.lower().find('a nao') > -1 or 
+    message.content.lower().find('ah nao') > -1):
+      await message.reply('https://tenor.com/vILU.gif');
 
 async def info(message):
     info_embed = discord.Embed(color=discord.Color.green())
